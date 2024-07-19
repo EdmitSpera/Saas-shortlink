b11c44da0b39dadc2dde923aeee74b11111afd42
EdmitSpera <laoxiao1886@gmail.com>
2024-07-19 20:59:13 +0800

    短链接分组分库分表

--- admit/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admit/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -3,6 +3,20 @@ package com.nageoffer.shortlink.admin.test;
 import org.h2.engine.User;
 
 public class UserTableShardingTest {
+    public static final String GroupSQL = "create table t_group_%d\n" +
+            "(\n" +
+            "    id          bigint auto_increment comment 'ID'\n" +
+            "        primary key,\n" +
+            "    gid         varchar(32)  null comment '分组标识',\n" +
+            "    name        varchar(64)  null comment '分组名称',\n" +
+            "    username    varchar(256) null comment '创建分组用户名',\n" +
+            "    sort_order  int          null comment '分组排序',\n" +
+            "    create_time datetime     null comment '创建时间',\n" +
+            "    update_time datetime     null comment '修改时间',\n" +
+            "    del_flag    tinyint(1)   null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_username_gid\n" +
+            "        unique (gid, username)\n" +
+            ");";
     public static final String UserSQL =
             "create table t_user_%d\n" +
                     "(\n" +
@@ -51,7 +65,7 @@ public class UserTableShardingTest {
     public static void main(String[] args) {
         // 进行分片操作，分16个片
         for (int i = 0; i < 16; i++) {
-            System.out.printf((LinkSQL) + "%n", i);
+            System.out.printf((GroupSQL) + "%n", i);
             System.out.println();
         }
     }
